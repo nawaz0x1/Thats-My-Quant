7cfecf016d367acf91312339e10190480ad1f4c5
Nawaz Haider <nawazhaider60@gmail.com>
2025-10-06 20:54:17 +0600

    Implement data loading and model architecture for image classification using TinyVGG

---
+++ learn_pytorch/moduler_approch/main.py
@@ -0,0 +1,31 @@
+import os
+import torch
+import data_setup, engine, model_builder, utils
+
+from torchvision import transforms
+
+# Setup hyperparameters
+NUM_EPOCHS = 5
+BATCH_SIZE = 32
+HIDDEN_UNITS = 10
+LEARNING_RATE = 0.001
+
+# Setup directories
+train_dir = "data/pizza_steak_sushi/train"
+test_dir = "data/pizza_steak_sushi/test"
+
+# Setup target device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Create transforms
+data_transform = transforms.Compose([
+  transforms.Resize((64, 64)),
+  transforms.ToTensor()
+])
+
+train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
+    train_dir=train_dir,
+    test_dir=test_dir,
+    transform=data_transform,
+    batch_size=BATCH_SIZE
+)
